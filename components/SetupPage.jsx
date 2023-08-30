5ada0a3a9ce001a8ec800d90b709ecd2880a810b
Jack Mitchell <jackmitchell2398@gmail.com>
2023-08-30 13:58:07 +0100

    Added process.env file

--- components/SetupPage.jsx
+++ components/SetupPage.jsx
@@ -11,7 +11,6 @@ import { storage } from "../src/firebase";
 import { ref, getDownloadURL } from "firebase/storage";
 import { collection, addDoc, setDoc, doc } from "firebase/firestore";
 
-
 function setupPage({ updateCurrentUser }) {
   // State to manage the register / sign up of usersa
   const profileRef = collection(db, "profiles");
@@ -127,178 +126,178 @@ function setupPage({ updateCurrentUser }) {
 
   return (
     <div>
-    <div className="setupHeader">
-      <h1 className="logo">Tweetpresso</h1>
+      <div className="setupHeader">
+        <h1 className="logo">Tweetpresso</h1>
       </div>
-    <div className="setupPageContainer ">
-      
-      <div className="backgroundSvg ">
-      <svg
-  version="1.1"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlnsXlink="http://www.w3.org/1999/xlink"
-  x="0px"
-  y="0px"
-  width="100%"
-  height="100%"
-  viewBox="0 0 1600 900"
-  preserveAspectRatio="xMidYMax slice"
->
-  <defs>
-    <linearGradient id="bg">
-      <stop offset="0%" stopColor="#d1a88f" />
-      <stop offset="50%" stopColor="#f4e1c1" />
-      <stop offset="100%" stopColor="#d1a88f" />
-    </linearGradient>
-    <path
-      id="wave"
-      fill="url(#bg)"
-      d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
+      <div className="setupPageContainer ">
+        <div className="backgroundSvg ">
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="100%"
+            height="100%"
+            viewBox="0 0 1600 900"
+            preserveAspectRatio="xMidYMax slice"
+          >
+            <defs>
+              <linearGradient id="bg">
+                <stop offset="0%" stopColor="#d1a88f" />
+                <stop offset="50%" stopColor="#f4e1c1" />
+                <stop offset="100%" stopColor="#d1a88f" />
+              </linearGradient>
+              <path
+                id="wave"
+                fill="url(#bg)"
+                d="M-363.852,502.589c0,0,236.988-41.997,505.475,0
   s371.981,38.998,575.971,0s293.985-39.278,505.474,5.859s493.475,48.368,716.963-4.995v560.106H-363.852V502.589z"
-    />
-  </defs>
-  <g>
-    <use xlinkHref="#wave" opacity=".3">
-      <animateTransform
-        attributeName="transform"
-        attributeType="XML"
-        type="translate"
-        dur="10s"
-        calcMode="spline"
-        values="270 230; -334 180; 270 230"
-        keyTimes="0; .5; 1"
-        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-        repeatCount="indefinite"
-      />
-    </use>
-    <use xlinkHref="#wave" opacity=".6">
-      <animateTransform
-        attributeName="transform"
-        attributeType="XML"
-        type="translate"
-        dur="8s"
-        calcMode="spline"
-        values="-270 230;243 220;-270 230"
-        keyTimes="0; .6; 1"
-        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-        repeatCount="indefinite"
-      />
-    </use>
-    <use xlinkHref="#wave" opacity=".9">
-      <animateTransform
-        attributeName="transform"
-        attributeType="XML"
-        type="translate"
-        dur="6s"
-        calcMode="spline"
-        values="0 230;-140 200;0 230"
-        keyTimes="0; .4; 1"
-        keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
-        repeatCount="indefinite"
-      />
-    </use>
-  </g>
-</svg>;
+              />
+            </defs>
+            <g>
+              <use xlinkHref="#wave" opacity=".3">
+                <animateTransform
+                  attributeName="transform"
+                  attributeType="XML"
+                  type="translate"
+                  dur="10s"
+                  calcMode="spline"
+                  values="270 230; -334 180; 270 230"
+                  keyTimes="0; .5; 1"
+                  keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                  repeatCount="indefinite"
+                />
+              </use>
+              <use xlinkHref="#wave" opacity=".6">
+                <animateTransform
+                  attributeName="transform"
+                  attributeType="XML"
+                  type="translate"
+                  dur="8s"
+                  calcMode="spline"
+                  values="-270 230;243 220;-270 230"
+                  keyTimes="0; .6; 1"
+                  keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                  repeatCount="indefinite"
+                />
+              </use>
+              <use xlinkHref="#wave" opacity=".9">
+                <animateTransform
+                  attributeName="transform"
+                  attributeType="XML"
+                  type="translate"
+                  dur="6s"
+                  calcMode="spline"
+                  values="0 230;-140 200;0 230"
+                  keyTimes="0; .4; 1"
+                  keySplines="0.42, 0, 0.58, 1.0;0.42, 0, 0.58, 1.0"
+                  repeatCount="indefinite"
+                />
+              </use>
+            </g>
+          </svg>
+          ;
+        </div>
+        {renderRegisterUser ? (
+          <>
+            <div className="registerContainer">
+              <h1 className="formH1">Create an Account</h1>
+              <input
+                placeholder="Username"
+                onChange={(event) =>
+                  setRegisterUser((prevData) => ({
+                    ...prevData,
+                    userName: event.target.value,
+                  }))
+                }
+              />
+              <input
+                placeholder="Email"
+                onChange={(event) =>
+                  setRegisterUser((prevData) => ({
+                    ...prevData,
+                    email: event.target.value,
+                  }))
+                }
+              />
+              <input
+                placeholder="Password"
+                onChange={(event) =>
+                  setRegisterUser((prevData) => ({
+                    ...prevData,
+                    password: event.target.value,
+                  }))
+                }
+              />
+
+              {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+              <div className="providersContainer">
+                <button className="formBtn" onClick={register}>
+                  Register
+                </button>
+
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/react-chat-app-7171d.appspot.com/o/assets%2Ficons8-google-60.png?alt=media&token=4f520ee6-afae-468f-9e8e-6d7ebcb59e35"
+                  className="icon"
+                  onClick={signInWithGoogle}
+                />
+              </div>
+              <p>
+                Already have an account?{" "}
+                <span className="signSpan" onClick={renderSignIn}>
+                  Sign In
+                </span>
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="signInContainer">
+              <h1 className="formH1">Sign in</h1>
+              <input
+                placeholder="Email"
+                onChange={(event) =>
+                  setSignInUser((prevData) => ({
+                    ...prevData,
+                    email: event.target.value,
+                  }))
+                }
+              />
+              <input
+                placeholder="Password"
+                onChange={(event) =>
+                  setSignInUser((prevData) => ({
+                    ...prevData,
+                    password: event.target.value,
+                  }))
+                }
+              />
+              {errorMessage && <p className="errorMessage">{errorMessage}</p>}
+
+              <div className="providersContainer">
+                <button className="formBtn" onClick={signIn}>
+                  Sign In
+                </button>
+
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/react-chat-app-7171d.appspot.com/o/assets%2Ficons8-google-60.png?alt=media&token=4f520ee6-afae-468f-9e8e-6d7ebcb59e35"
+                  className="icon"
+                  onClick={signInWithGoogle}
+                />
+              </div>
+              <p>
+                Don't have an account?{" "}
+                <span className="signSpan" onClick={renderSignUp}>
+                  Sign Up
+                </span>
+              </p>
+            </div>
+          </>
+        )}
       </div>
-      {renderRegisterUser ? (
-        <>
-          <div className="registerContainer">
-            <h1 className="formH1">Create an Account</h1>
-            <input
-              placeholder="Username"
-              onChange={(event) =>
-                setRegisterUser((prevData) => ({
-                  ...prevData,
-                  userName: event.target.value,
-                }))
-              }
-            />
-            <input
-              placeholder="Email"
-              onChange={(event) =>
-                setRegisterUser((prevData) => ({
-                  ...prevData,
-                  email: event.target.value,
-                }))
-              }
-            />
-            <input
-              placeholder="Password"
-              onChange={(event) =>
-                setRegisterUser((prevData) => ({
-                  ...prevData,
-                  password: event.target.value,
-                }))
-              }
-            />
-
-            {errorMessage && <p className="errorMessage">{errorMessage}</p>}
-            <div className="providersContainer">
-              <button className="formBtn" onClick={register}>
-                Register
-              </button>
-
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/react-chat-app-7171d.appspot.com/o/assets%2Ficons8-google-60.png?alt=media&token=4f520ee6-afae-468f-9e8e-6d7ebcb59e35"
-                className="icon"
-                onClick={signInWithGoogle}
-              />
-            </div>
-            <p>
-              Already have an account?{" "}
-              <span className="signSpan" onClick={renderSignIn}>
-                Sign In
-              </span>
-            </p>
-          </div>
-        </>
-      ) : (
-        <>
-          <div className="signInContainer">
-            <h1 className="formH1">Sign in</h1>
-            <input
-              placeholder="Email"
-              onChange={(event) =>
-                setSignInUser((prevData) => ({
-                  ...prevData,
-                  email: event.target.value,
-                }))
-              }
-            />
-            <input
-              placeholder="Password"
-              onChange={(event) =>
-                setSignInUser((prevData) => ({
-                  ...prevData,
-                  password: event.target.value,
-                }))
-              }
-            />
-            {errorMessage && <p className="errorMessage">{errorMessage}</p>}
-
-            <div className="providersContainer">
-              <button className="formBtn" onClick={signIn}>
-                Sign In
-              </button>
-
-              <img
-                src="https://firebasestorage.googleapis.com/v0/b/react-chat-app-7171d.appspot.com/o/assets%2Ficons8-google-60.png?alt=media&token=4f520ee6-afae-468f-9e8e-6d7ebcb59e35"
-                className="icon"
-                onClick={signInWithGoogle}
-              />
-            </div>
-            <p>
-              Don't have an account?{" "}
-              <span className="signSpan" onClick={renderSignUp}>
-                Sign Up
-              </span>
-            </p>
-          </div>
-        </>
-      )}
     </div>
-    </div>
-  )
+  );
 }
 
 export default setupPage;
--- src/firebase.js
+++ src/firebase.js
@@ -1,28 +1,27 @@
-
 import { initializeApp } from "firebase/app";
-import  { getAuth } from 'firebase/auth';
-import { getFirestore} from 'firebase/firestore'
-import {getStorage} from 'firebase/storage';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// May need to hide these in production 
+require('dotenv').config();
+
 const firebaseConfig = {
-    apiKey: "AIzaSyA1xk4xg-D7jvfMSB6eUu_YnBbHfzt8QCM",
-    authDomain: "react-chat-app-7171d.firebaseapp.com",
-    projectId: "react-chat-app-7171d",
-    storageBucket: "react-chat-app-7171d.appspot.com",
-    messagingSenderId: "246024372940",
-    appId: "1:246024372940:web:1588e609575407d40cb869"
-  };
-  
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 
-  // Initialize authentication
-  export const auth = getAuth(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Initialize firestore (database)
-  export const db = getFirestore()
+// Initialize authentication
+export const auth = getAuth(app);
 
-  // Initialize Storage
-  export const storage = getStorage()
- 
+// Initialize firestore (database)
+export const db = getFirestore();
+
+// Initialize Storage
+export const storage = getStorage();
